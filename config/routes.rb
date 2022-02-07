bd4514c8736b926aca54b4da29eb07f4507bd89c
Andy Maleh <andy.am@gmail.com>
2022-02-07 16:19:32 -0500

    Configure routes for glimmer-dsl-opal Rails Engine

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  mount Glimmer::Engine => "/glimmer" # add on top
   resources :welcomes
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root to: 'welcomes#index'
 end
