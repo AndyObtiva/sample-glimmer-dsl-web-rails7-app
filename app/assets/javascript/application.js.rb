9783188660558ac02a9fa343d5456b07acae9ab0
Andy Maleh <andy.am@gmail.com>
2023-12-30 16:33:28 -0500

    Upgrade to glimmer-dsl-web 0.0.5 adding hello_form.rb

--- app/assets/javascript/application.js.rb
+++ app/assets/javascript/application.js.rb
@@ -3,6 +3,7 @@ require 'glimmer-dsl-web'
 # To load a sample, enable one of the following lines and disable everything that follows
 # require 'glimmer-dsl-web/samples/hello/hello_world.rb'
 # require 'glimmer-dsl-web/samples/hello/hello_button.rb'
+# require 'glimmer-dsl-web/samples/hello/hello_form.rb'
 
 require 'puts_debuggerer'
 
